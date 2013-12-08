9660e3b578f13b252b119d664f85c626063ac954
Laurent Bachelier <laurent@bachelier.name>
2013-12-08 01:31:28 +0100

    Remove eix limit in terminal

--- tools/eix.sh
+++ tools/eix.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd $(dirname $0)/..
+export EIX_LIMIT=0
 export EIXRC=${PWD}/tools/eixrc
 export EIX_CACHEFILE="${PWD}/tools/eix.db"
 PORTDIR_OVERLAY="" eix-update -x "*portage*" -a "${PWD}" -o "${EIX_CACHEFILE}" -q
